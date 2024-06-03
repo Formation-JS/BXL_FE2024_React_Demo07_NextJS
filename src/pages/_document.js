17928910e565b562cc74b7c38bc7711a9463245d
Aurélien St <aurelien.strimelle@bstorm.be>
2024-06-03 15:27:00 +0200

    App squeleton ☠

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,8 +1,12 @@
+//! Composant « Document » : 
+//  - Définir le sequlette de base de la page
+//  - Ne pas interagir avec le routing de NextJS (Pas d'utilisation de la balise "Link")
+
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="fr">
       <Head />
       <body>
         <Main />
